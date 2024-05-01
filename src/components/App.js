8e3c10cd6febfb746cb32a67c3c37556f2de6cd5
lizwachira <lizwanguwachira@gmail.com>
2024-05-02 01:44:10 +0300

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import About from "../components/About";
 
 function App() {
   return (
